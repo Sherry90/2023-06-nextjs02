53bd8174f44c5264150126421237e9219df7fb3d
Sherry90 <hhlsdkm@gmail.com>
2023-07-08 18:34:36 +0900

    add EventItem css module

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 
+import classes from "./EventItem.module.css";
+import Button from "../ui/Button";
+
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
 
@@ -12,20 +15,20 @@ const EventItem = (props) => {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
-          <Link href={exploreLink}>Explore Event</Link>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>Explore Event</Button>
         </div>
       </div>
     </li>
